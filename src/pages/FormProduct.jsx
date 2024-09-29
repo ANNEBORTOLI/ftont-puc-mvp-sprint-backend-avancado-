dddf85dcd4c6d1c1b5e6bfc3fe51782d656d6cc7
Anne Bortoli <annebortoli@gmail.com>
2024-09-29 01:54:31 -0300

    Add Edit Product Page

--- src/pages/FormProduct.jsx
+++ src/pages/FormProduct.jsx
@@ -3,29 +3,88 @@ import { Footer } from "../components/Footer";
 import styles from "./FormProduct.module.css";
 import { useShoppingList } from "../contexts/ShoppingListContext";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export function FormProduct() {
   const { shoppingList } = useShoppingList();
+  const navigate = useNavigate();
   const [product, setProduct] = useState({
-    id: 0,
+    id: "",
     title: "",
     size: "",
-    price: 0
+    price: ""
   });
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const action = params.get("action");
-  const submitLabel = action == "add" ? "Adicionar" : "Editar";
+  const submitLabel = action == "edit" ? "Editar" : "Adicionar";
   const id = params.get("id");
 
   useEffect(() => {
     if (action == "edit") {
-      // fetch no BD
+      let url = `http://localhost:5000/product?id=${id}`;
+      fetch(url, {
+        method: "GET"
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setProduct(data);
+        })
+        .catch((error) => {
+          console.error("Erro: ", error);
+        });
     }
   }, []);
 
-  const handleSubmit = () => {};
+  const isNumeric = (string) => string == Number.parseFloat(string);
+
+  const validate = () => {
+    return (
+      document.getElementById("title").value != "" &&
+      document.getElementById("size").value != "" &&
+      document.getElementById("price").value != "" &&
+      isNumeric(document.getElementById("price").value)
+    );
+  };
+
+  const montaFormData = () => {
+    let formData = new URLSearchParams();
+
+    if (action == "edit") {
+      formData.append("id", id);
+    }
+
+    formData.append("title", document.getElementById("title").value);
+    formData.append("size", document.getElementById("size").value);
+    formData.append("price", document.getElementById("price").value);
+    formData.append("description", "");
+
+    return formData;
+  };
+
+  const handleSubmit = () => {
+    if (!validate()) {
+      document.getElementById("mensagem").innerHTML =
+        "Todos os campos são obrigatórios! Preço deve ser numérico!";
+    } else {
+      let formData = montaFormData();
+
+      let url = "http://localhost:5000/product";
+      let metodo = action == "edit" ? "PUT" : "POST";
+
+      fetch(url, {
+        method: metodo,
+        body: formData
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          navigate("/admin");
+        })
+        .catch((error) => {
+          console.error("Erro: ", error);
+        });
+    }
+  };
 
   return (
     <>
@@ -33,12 +92,22 @@ export function FormProduct() {
       <form className={styles.form} id="formulario">
         <div className={styles.formGroup}>
           <label htmlFor="title">Nome:</label>
-          <input type="text" name="title" id="title" value={product.nome} />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            defaultValue={product.title}
+          />
         </div>
 
         <div className={styles.formGroup}>
           <label htmlFor="size">Tamanho:</label>
-          <input type="text" name="size" id="size" value={product.size} />
+          <input
+            type="text"
+            name="size"
+            id="size"
+            defaultValue={product.size}
+          />
         </div>
 
         <div className={styles.formGroup}>
@@ -47,14 +116,15 @@ export function FormProduct() {
             type="text"
             name="price"
             id="price"
-            value={product.price}
+            defaultValue={product.price}
             placeholder="0.00"
           />
         </div>
 
-        <button className={styles.submitButton} onClick={handleSubmit}>
+        <div className={styles.submitButton} onClick={handleSubmit}>
           {submitLabel}
-        </button>
+        </div>
+        <div className={styles.message} id="mensagem"></div>
       </form>
       <Footer />
     </>
